6c6b74b8bf2afb9a740fcda19b52312f13ac4a7e
Hale <mahongzhi009@gmail.com>
2016-12-22 16:50:25 +0000

    basic bluetooth

--- app/src/main/java/com/project/hale/messgaesender/MainActivity.java
+++ app/src/main/java/com/project/hale/messgaesender/MainActivity.java
@@ -1,9 +1,14 @@
 package com.project.hale.messgaesender;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -15,16 +20,78 @@ import com.peak.salut.SalutServiceData;
 import com.project.hale.messgaesender.Wifi.SenderDevice;
 import com.project.hale.messgaesender.Wifi.WifiBoardCastManager;
 
+import java.util.List;
+
+import top.wuhaojie.bthelper.BtHelperClient;
+import top.wuhaojie.bthelper.MessageItem;
+import top.wuhaojie.bthelper.OnSearchDeviceListener;
+import top.wuhaojie.bthelper.OnSendMessageListener;
+
 public class MainActivity extends AppCompatActivity implements DeviceListFragment.OnFragmentInteractionListener, SalutDataCallback {
     WifiP2pManager mManager;
     WifiP2pManager.Channel mChannel;
     WifiBoardCastManager wm = WifiBoardCastManager.getsInstance();
+    BtHelperClient btHelperClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Android 6.0 Bluetooth
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String permissions[] = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 5230);
+        }
+        //enable bluetooth discoverable
+        Intent discoverableIntent = new
+                Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+
+
         setContentView(R.layout.activity_main);
-        initwifi();
+        btHelperClient = BtHelperClient.from(MainActivity.this);
+        btHelperClient.searchDevices(new OnSearchDeviceListener() {
+            @Override
+            public void onStartDiscovery() {
+                Log.d("BT","onStart");
+            }
+
+            @Override
+            public void onNewDeviceFounded(BluetoothDevice bluetoothDevice) {
+                Log.d("BT","onFound"+bluetoothDevice.getAddress());
+                MessageItem mi=new MessageItem("Messageoasfklajsasdhfkajsfdhk");
+                btHelperClient.sendMessage(bluetoothDevice.getAddress(), mi, true, new OnSendMessageListener() {
+                    @Override
+                    public void onSuccess(int i, String s) {
+                        Log.d("BT","onSuccess"+i+" "+s);
+                    }
+
+                    @Override
+                    public void onConnectionLost(Exception e) {
+                        Log.d("BT","onConnectionLost");
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.d("BT","onError"+e);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onSearchCompleted(List<BluetoothDevice> bondedList, List<BluetoothDevice> newList) {
+                Log.d("BT", "SearchCompleted: bondedList" + bondedList.toString());
+                Log.d("BT", "SearchCompleted: newList" + newList.toString());
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.d("BT","onError"+e);
+            }
+        });
+   //     initwifi();
 
 
     }
